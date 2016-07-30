fc413a7f81faacf8b80afaf87a59bd616f22daae
donaldwakefield <don.wakefield@gmail.com>
2016-07-30 08:59:38 -0700

    Sites and Circles have header sketches, anticipated to change when we add the DataStructures dir

---
+++ Primitives/Circle.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <Site.h>
+
+/*!
+ * \class Circle is represented by three sites. It will likely be
+ *        changed to refer to Sites outside itself, but this gets us
+ *        started.
+ * \brief We'd like to enforce some invariants, such as
+ *        - the Sites being montonic in x
+ *        - the Circle's lowest y being below any of the Sites (so
+ *          upward curving Sites being disallowed)
+ *
+ *        I really don't like the idea of introducing exceptions into
+ *        this design, so the constructor is probably not the right
+ *        place to do this. A predicate method is more likely.
+ */
+class Circle : private boost::noncopyable
+{
+public:
+
+   explicit Circle(const Site &a, const Site &b, const Site &c) : a_(a), b_(b), c_(c) {};
+   ~Circle() {};
+
+   bool valid() { return true; }
+
+private:
+
+   Site a_;
+   Site b_;
+   Site c_;
+};
---
+++ Primitives/Site.h
@@ -0,0 +1,26 @@
+#pragma once
+
+// We will use our /usr/local/lib boost install for prototyping
+//
+#include <boost/noncopyable.hpp>
+
+/*!
+ * \class Site represents a single point in the Voronoi
+ *        diagram. Coordinates are in real number units.
+ *
+ * \brief It is not clear yet how tightly we want to control copying
+ *        or indeed default construction, as these will likely be
+ *        going into the priority queue by value...
+ */
+class Site : private boost::noncopyable
+{
+public:
+
+   explicit Site(double x, double y) : x_(x), y_(y) {};
+   ~Site() {};
+
+private:
+
+   double x_;
+   double y_;
+};
